e139625b9cb45cf7ef7643a4b970146877156347
Jhon Builes <83386176+jfbz14@users.noreply.github.com>
2023-04-08 20:27:02 -0500

    content in template technical

--- web_guiatec/urls.py
+++ web_guiatec/urls.py
@@ -5,5 +5,5 @@ from .views import TemplateIndex, TemplateTechnicalSupport
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateIndex.as_view(), name='index'),
-    path('soporte_tecnico/', TemplateTechnicalSupport.as_view(), name='technical_support'),
+    path('soporte-tecnico-de-computadoras/', TemplateTechnicalSupport.as_view(), name='technical_support'),
 ]
