30cfdf11cfa8a9519e2331a05013e422412b3d6d
Jhon Builes <83386176+jfbz14@users.noreply.github.com>
2023-04-09 15:15:58 -0500

    ALLOWED_HOSTS

--- web_guiatec/settings.py
+++ web_guiatec/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-pff3hsq-w(rv_5x=ef&1p+!1$y+xh_cf4bile@iajk^4r_(ir$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
