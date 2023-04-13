ba7d77d5e6e705ea0ad5a916d47518dc947420c5
Jhon Builes <83386176+jfbz14@users.noreply.github.com>
2023-04-13 15:36:05 -0500

    ALLOWED_HOSTS update2

--- web_guiatec/settings.py
+++ web_guiatec/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env.list('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+print(ALLOWED_HOSTS)
 
 
 # Application definition
